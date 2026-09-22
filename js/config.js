03be11b522b910e94d8ee2776a5910b06e0a29bd
natapolc-ux <natapol.c@chs.ac.th>
2026-09-22 14:08:52 +0700

    Add STATUS_URL for ClassFlow V2

--- js/config.js
+++ js/config.js
@@ -2,3 +2,4 @@
 const API_URL = 'https://script.google.com/macros/s/AKfycbwNY_ojt0kczg3zbcdypJ2g0C9HMm1R9agnzqGDl-O0Mm6h1Q3FjJ-V92uFb95eLtYFdw/exec';
 const TEACHER_URL = 'https://natapolc-ux.github.io/ClassFlow.V2-teacher/';
 const STUDENT_URL = 'https://natapolc-ux.github.io/ClassFlow.V2-Student/';
+const STATUS_URL = 'https://natapolc-ux.github.io/ClassFlow.V2-status/';
